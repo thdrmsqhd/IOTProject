aa95fe77148183e5b41692be3ead6f5235ef5f88
송근봉 <thdrmsqhd@gmail.com>
2023-07-11 21:12:50 +0900

    try catch 추가

--- camera.py
+++ camera.py
@@ -55,18 +55,21 @@ class Camara:
             frame = cv2.flip(frame, 1)  # 좌우 대칭
 
             # 프레임을 인코딩하여 JPEG 파일 형식으로 변환
-            _, buffer = cv2.imencode('.jpg', frame)
+            try:
+                _, buffer = cv2.imencode('.jpg', frame)
 
-            # 이미지 데이터를 base64 문자열로 인코딩
-            jpg_as_text = base64.b64encode(buffer).decode('utf-8')
+                # 이미지 데이터를 base64 문자열로 인코딩
+                jpg_as_text = base64.b64encode(buffer).decode('utf-8')
 
-            # 소켓으로 html에 화면을 전송한다.
-            socketio.emit("streaming", {"frame": jpg_as_text})
-            if self.sense is True:
-                print("it work")
-                self.capture(jpg_as_text, app, socketio)
-                self.sense = False
-                #    AI_분석_함수() # 만약 True라면 plugOff, socketio
-                plugService.plugOff()
-                socketio.emit("plugStatus", {"plugStatus": plugService.plugStatus()})
+                # 소켓으로 html에 화면을 전송한다.
+                socketio.emit("streaming", {"frame": jpg_as_text})
+                if self.sense is True:
+                    print("it work")
+                    self.capture(jpg_as_text, app, socketio)
+                    self.sense = False
+                    #    AI_분석_함수() # 만약 True라면 plugOff, socketio
+                    plugService.plugOff()
+                    socketio.emit("plugStatus", {"plugStatus": plugService.plugStatus()})
+            except Exception as e:
+                pass
         pass
